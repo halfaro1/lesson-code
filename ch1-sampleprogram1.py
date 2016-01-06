9ef3663ce7ec058c21a94e41d07876656a093459
Hector Alfaro <halfaro1@valenciacollege.edu>
2016-01-06 12:38:18 -0500

    initial commit

---
+++ ch1-sampleprogram1.py
@@ -0,0 +1,12 @@
+#this example shows the basics of if and else statements
+if spam == 1:
+    #notice how the body of the loop is indented
+	print('Hello')
+#elif is short for "else, if"
+elif spam == 2:
+	print('Howdy')
+#the else portion is executed if neither the if, nor elif portions are executed
+else:
+	print('Greetings!')
+    
+#note: this standalone code will not run as the variable spam is not defined
