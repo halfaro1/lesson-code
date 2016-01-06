326591245d3258e04dbe2c1c991cf3dd8870de7c
Hector Alfaro <halfaro1@valenciacollege.edu>
2016-01-06 12:47:42 -0500

    initial commit

---
+++ ch1-sampleprogram2.py
@@ -0,0 +1,3 @@
+#this program prints all the numbers from 1 to 10 using a for-loop
+for i in range(1, 11):
+    print(i)
---
+++ ch1-sampleprogram3.py
@@ -0,0 +1,5 @@
+#this program prints all the numbers from 1 to 10 using a while-loop
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
